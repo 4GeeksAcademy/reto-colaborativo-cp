654a1ae6226bd87907393f827ba1eedbd6d13784
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2026-04-17 23:59:22 +0000

    ft catalogo

---
+++ producto.js
@@ -0,0 +1,36 @@
+async function cargarProductos() {
+    try {
+        const res = await fetch('./productos.json');
+        const productos = await res.json();
+
+        const grid = document.getElementById('productosGrid');
+
+        const primeros20 = productos.slice(0, 20);
+
+        primeros20.forEach(prod => {
+            const card = document.createElement('div');
+            card.className = "group cursor-pointer border border-gray-200 rounded-xl p-3 hover:shadow-md transition";
+
+            card.innerHTML = `
+                <div class="bg-gray-100 rounded-xl overflow-hidden">
+                    <img src="${prod.imagen}" 
+                         alt="${prod.nombre}" 
+                         class="w-full h-72 object-cover group-hover:scale-105 transition duration-300">
+                </div>
+
+                <div class="mt-3">
+                    <h3 class="text-sm font-medium">${prod.nombre}</h3>
+                    <p class="text-gray-500 text-sm">$${prod.precio}</p>
+                </div>
+            `;
+
+            grid.appendChild(card);
+        });
+
+    } catch (error) {
+        console.error("Error cargando productos:", error);
+    }
+}
+
+// 👇 importante: esperar a que cargue el DOM
+document.addEventListener("DOMContentLoaded", cargarProductos);
